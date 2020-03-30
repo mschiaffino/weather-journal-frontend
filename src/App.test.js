2c3e21b579218fb677728d0ee64a78fca905e532
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-03-30 16:17:54 -0300

    Remove unneeded boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders weather journal title", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = getByText(/weather journal/i);
+  expect(title).toBeInTheDocument();
 });
