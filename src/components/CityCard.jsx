37076303d977aa83f9fcdf6fc67d83eddc2fdc4d
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-03-30 17:36:00 -0300

    Add label and styles

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -4,8 +4,13 @@ import './CityCard.css';
 export default function CityCard({ name, wind }) {
   return (
     <div className='city-card'>
-      <span>{name}</span>
-      <span>{wind}</span>
+      <div className='wind-info-label'>
+        <span>Wind Speed</span>
+      </div>
+      <div className='city-card-info'>
+        <span>{name}</span>
+        <span>{wind}</span>
+      </div>
     </div>
   );
 }
