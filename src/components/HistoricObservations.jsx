e89a598a8fea3a7e66672544e2042269057a0dc6
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-03-31 16:31:26 -0300

    Add observations timestamps

--- src/components/HistoricObservations.jsx
+++ src/components/HistoricObservations.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import dayjs from 'dayjs';
 
 import './HistoricObservations.css';
+
+const formatDatetime = date => dayjs(date).format('HH:mm MM/DD/YYYY');
 
 export default function HistoricObservations({ observations = [] }) {
   return (
@@ -8,9 +11,14 @@ export default function HistoricObservations({ observations = [] }) {
       <span className='historic-observations-title'>Historic observations</span>
       <div className='historic-observations-list'>
         {!observations.length && <span>No observations yet.</span>}
-        {observations.map(({ id, text }) => (
-          <div key={id}>
-            <span>{text}</span>
+        {observations.map(({ id, datetime, text }) => (
+          <div className='observation-row' key={id}>
+            <div className='observation-datetime'>
+              <span>{formatDatetime(datetime)}</span>
+            </div>
+            <div>
+              <span>{text}</span>
+            </div>
           </div>
         ))}
       </div>
